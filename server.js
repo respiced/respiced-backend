d1e223393350653dd26f6fb9d7d9ba2574e87e5b
respiced <respiced@outlook.com>
2025-02-08 06:33:17 +0000

    12

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
 app.use(express.json());
+const cors = require("cors");
+app.use(cors());
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
